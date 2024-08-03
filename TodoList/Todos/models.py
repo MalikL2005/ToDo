0058f7176773f908f5996cf000d5e7f4036df1da
MalikL2005 <malik.lorenz2005@gmail.com>
2024-08-03 14:18:58 +0200

    feat:functionality of Todo-App

--- TodoList/Todos/models.py
+++ TodoList/Todos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class ToDoItem(models.Model):
+    title = models.CharField(max_length=100, default="New ToDo-Item")
+    description = models.CharField(max_length=500)
+    checked = models.BooleanField(default=False)
--- TodoList/Todos/views.py
+++ TodoList/Todos/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoItem
 
 # Create your views here.
 def display_todos(req): 
-    return HttpResponse("Display todos here")
+    todo_list = list(ToDoItem.objects.all().values())
+    print(list(todo_list))
+    return render(req, 'todos.html', {'todo_list': todo_list})
