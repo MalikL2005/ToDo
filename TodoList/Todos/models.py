59775e115745eeb5051c8543ecf8fb574bfb0fd4
MalikL2005 <malik.lorenz2005@gmail.com>
2024-08-03 14:27:09 +0200

    feat: address added to Todo-Model

--- TodoList/Todos/models.py
+++ TodoList/Todos/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class ToDoItem(models.Model):
     title = models.CharField(max_length=100, default="New ToDo-Item")
     description = models.CharField(max_length=500)
+    address = models.CharField(max_length=500, null=True)
     checked = models.BooleanField(default=False)
