2f8de843c481e6fc386a6847b7875b7a5160c836
MalikL2005 <malik.lorenz2005@gmail.com>
2024-08-03 15:21:34 +0200

    feat: add new items

--- TodoList/Todos/views.py
+++ TodoList/Todos/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoItem
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-def display_todos(req): 
+@csrf_exempt
+def display_todos(req):
+    if req.method == 'POST': 
+        try: new_item = ToDoItem.objects.create(title=req.POST['title'], description=req.POST['description'], address=req.POST['address'])
+        except: print("Could not add item to db")
     todo_list = list(ToDoItem.objects.all().values())
-    print(list(todo_list))
     return render(req, 'todos.html', {'todo_list': todo_list})
+
+
