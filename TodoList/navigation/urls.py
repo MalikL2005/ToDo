6d6b99f70e5398853bec8030a07d136aaa26d3ef
MalikL2005 <malik.lorenz2005@gmail.com>
2024-08-05 13:38:07 +0200

    fix: navigation routing

--- TodoList/navigation/urls.py
+++ TodoList/navigation/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     path('', views.start_nav),
     path('items/<int:pk>', views.navigate),
+    path('items/<int:pk>/delete', views.remove_todo_item),
 ]
--- TodoList/navigation/views.py
+++ TodoList/navigation/views.py
@@ -6,16 +6,24 @@ import os
 # Create your views here.
 
 def start_nav(req): 
-    items = ToDoItem.objects.all().values_list()
-    item=items[0]
+    try:
+        items = ToDoItem.objects.all().values_list()
+        item=items[0]
+    except: 
+        return HttpResponsePermanentRedirect(os.environ['URL']) 
     return HttpResponseRedirect(f'items/{item[4]}')
 
 def navigate(req, pk):
-    item = ToDoItem.objects.get(id=pk)
-    url = os.environ['URL']
+    item = ToDoItem.objects.get(id=pk) 
     start= ''
     destination = ''
     maps_url = f'https://www.google.com/maps/dir/{start}/{destination}'
     print(maps_url)
-    context = {'url': f'{url}nav/', 'item': item, 'maps_url': maps_url}
+    context = {'item': item, 'maps_url': maps_url}
     return render(req, 'nav_base.html', context)
+
+def remove_todo_item(req, pk):
+    item_to_delete = ToDoItem.objects.filter(id=pk).delete()
+    print(item_to_delete)
+    return HttpResponsePermanentRedirect(os.environ['URL']+'/nav')
+
