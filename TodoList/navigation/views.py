4072d55cb22f6cb94901d3063a97ba60e4a3c411
MalikL2005 <malik.lorenz2005@gmail.com>
2024-08-06 14:32:31 +0200

    fix: rerouting of todo-start

--- TodoList/navigation/views.py
+++ TodoList/navigation/views.py
@@ -3,30 +3,36 @@ from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.http import HttpResponsePermanentRedirect
 from Todos.models import ToDoItem
+from django.contrib import messages
 import os
 # Create your views here.
 
 def start_nav(req): 
-    print("Starting")
     try: 
         items = ToDoItem.objects.all().values_list()
         item=items[0]
-    except:
+        print(item)
+    except: 
+        #pop up alert (No todos)
+        print("No Todos Here")
+        
         return HttpResponsePermanentRedirect(os.environ['URL'])
-    print(item)
-    return HttpResponseRedirect(f'items/{item[4]}')
+    return HttpResponsePermanentRedirect(f'items/{item[4]}')
 
 def navigate(req, pk):
-    item = ToDoItem.objects.get(id=pk) 
-    start= ''
-    destination = ''
-    maps_url = f'https://www.google.com/maps/dir/{start}/{destination}'
-    print(maps_url)
-    context = {'item': item, 'maps_url': maps_url}
+    try: 
+        item = ToDoItem.objects.get(id=pk) 
+        start= ''
+        destination = ''
+        maps_url = f'https://www.google.com/maps/dir/{start}/{destination}'
+        # print(maps_url)
+        context = {'item': item, 'maps_url': maps_url}
+    except:
+        return HttpResponsePermanentRedirect(os.environ['URL'])
     return render(req, 'nav_base.html', context)
 
 def remove_todo_item(req, pk):
-    item_to_delete = ToDoItem.objects.filter(id=pk).delete()
+    item_to_delete = ToDoItem.objects.filter(pk=pk).delete()
     print(item_to_delete)
-    return HttpResponsePermanentRedirect((os.environ['URL']+'/nav'))
-
+    return HttpResponsePermanentRedirect(os.environ['URL']+'nav')
+   
