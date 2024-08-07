fe7b898e460e5560673b26eab3d99e4f487d26c1
MalikL2005 <malik.lorenz2005@gmail.com>
2024-08-07 23:47:30 +0200

    fix: address is now required; map now directs to place of destination

--- TodoList/navigation/views.py
+++ TodoList/navigation/views.py
@@ -15,17 +15,21 @@ def start_nav(req):
     except: 
         #pop up alert (No todos)
         print("No Todos Here")
-        
         return HttpResponsePermanentRedirect(os.environ['URL'])
     return HttpResponsePermanentRedirect(f'items/{item[4]}')
 
 def navigate(req, pk):
     try: 
         item = ToDoItem.objects.get(id=pk) 
-        start= ''
-        destination = ''
-        maps_url = f'https://www.google.com/maps/dir/{start}/{destination}'
-        # print(maps_url)
+        print(item.address)
+        if item.address:
+            destination = item.address.replace(' ', '+')
+        start= 'Poppelsdorfer+Schloss,+Meckenheimer+Allee+171,+53115+Bonn'.replace(' ', '+')
+        API_KEY = os.environ['API_KEY_TWO']
+        # maps_url = f'https://www.google.com/maps/dir/Hobsweg+64,+53125+Bonn,+Deutschland/Am+Alten+Stauwehr+1,+53340+Meckenheim,+Deutschland/@50.6682948,6.965657,12z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x47bee25a746bb87f:0xbb419df7fd43f7ef!2m2!1d7.0755759!2d50.6794474!1m5!1m1!1s0x47bf1d549bf1f6bd:0x2e9f5e644fb89c87!2m2!1d7.0195342!2d50.6360963!3e0?hl=de-DE&entry=ttu'
+        # maps_url = f'https://www.google.com/maps/dir/?key={API_KEY}&origin={start}&destination={destination}'
+        maps_url = f'https://www.google.com/maps/embed/v1/place?key={API_KEY}&q={destination}'
+        print(maps_url)
         context = {'item': item, 'maps_url': maps_url}
     except:
         return HttpResponsePermanentRedirect(os.environ['URL'])
